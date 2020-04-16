0ddc2b8116fa5dba02cd40858566058da3fbf29b
Mohamed Rached Bouraoui <mrbouraoui@gmail.com>
2020-04-15 22:44:52 -0400

    End of section -10

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/iProduct';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: IProduct;
+  constructor(private shopService: ShopService,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+
+    const productId = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.shopService.getProduct(productId).subscribe((product: IProduct) => {
+
+      this.product = product;
+
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+
+}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,35 +5,22 @@ import { IPagination } from '../shared/models/iPagination';
 import { IBrand } from '../shared/models/iBrand';
 import { IProductType } from '../shared/models/iProductType';
 import { ShopParams } from '../shared/models/shopParams';
+import { Observable } from 'rxjs';
+import { IProduct } from '../shared/models/iProduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  productsBaseUrl = 'https://localhost:5001/api/products';
+  productsBaseUrl = 'https://localhost:5001/api/products/';
 
   constructor(private httpClient: HttpClient) {
 
   }
 
   getProducts(shopParams: ShopParams) {
-    let httpParams = new HttpParams();
-
-    httpParams = httpParams.append('pageIndex', shopParams.pageIndex.toString());
-    httpParams = httpParams.append('pageSize', shopParams.pageSize.toString());
-
-    httpParams = httpParams.append('sortBy', shopParams.sortBy);
-
-    if (shopParams.brandId && shopParams.brandId > 0) {
-      httpParams = httpParams.append('brandId', shopParams.brandId.toString());
-    }
-    if (shopParams.productTypeId && shopParams.productTypeId > 0) {
-      httpParams = httpParams.append('typeId', shopParams.productTypeId.toString());
-    }
-    if (shopParams.searchBy && shopParams.searchBy !== '') {
-      httpParams = httpParams.append('searchBy', shopParams.searchBy);
-    }
+    const httpParams = this.setupHttpParams(shopParams);
 
     // this will give us the HttpResponse instead of the body's response
     const result = this.httpClient.get<IPagination>(this.productsBaseUrl, { observe: 'response', params: httpParams })
@@ -45,11 +32,34 @@ export class ShopService {
     return result;
   }
 
+  private setupHttpParams(shopParams: ShopParams) {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('pageIndex', shopParams.pageIndex.toString());
+    httpParams = httpParams.append('pageSize', shopParams.pageSize.toString());
+    httpParams = httpParams.append('sortBy', shopParams.sortBy);
+    if (shopParams.brandId && shopParams.brandId > 0) {
+      httpParams = httpParams.append('brandId', shopParams.brandId.toString());
+    }
+    if (shopParams.productTypeId && shopParams.productTypeId > 0) {
+      httpParams = httpParams.append('typeId', shopParams.productTypeId.toString());
+    }
+    if (shopParams.searchBy && shopParams.searchBy !== '') {
+      httpParams = httpParams.append('searchBy', shopParams.searchBy);
+    }
+    return httpParams;
+  }
+
+  getProduct(productId: number): Observable<IProduct> {
+
+    return this.httpClient.get<IProduct>(this.productsBaseUrl + `${productId}`);
+  }
+
   getBrands() {
-    return this.httpClient.get<IBrand[]>(this.productsBaseUrl + '/brands');
+    return this.httpClient.get<IBrand[]>(this.productsBaseUrl + 'brands');
   }
 
   getProductTypes() {
-    return this.httpClient.get<IProductType[]>(this.productsBaseUrl + '/types');
+    return this.httpClient.get<IProductType[]>(this.productsBaseUrl + 'types');
   }
+
 }
